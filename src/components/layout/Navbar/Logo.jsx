1238384fa984eb4edcb6b2219f537230647b34a4
nou44 <draouiy22@gmail.com>
2026-08-31 00:28:03 +0200

    optimize logo image

--- src/components/layout/Navbar/Logo.jsx
+++ src/components/layout/Navbar/Logo.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-import logo from "../../../assets/images/logo.png";
+import logo from "../../../assets/images/kadecho-logo-256.webp";
 
 export default function Logo() {
   return (
